daac4671b806df6f0d640267be1e5b2ca6c5e3d1
morningsun <chenyang198302@163.com>
2018-03-28 14:02:42 +0800

    test

--- ExhibitionWebApp/index1.py
+++ ExhibitionWebApp/index1.py
@@ -19,21 +19,6 @@ jq('#datepicker').datetimepicker({'format': 'yyyy-mm',
                                   'todayHighlight': 'true',
                                   'todayBtn': 'true'})
 
-class item_info(expo_info):
-    def gen_li(self):
-        item_title = HTML.A(self.show,href=self.url)
-        item_text1  = HTML.DIV('展馆：{}'.format(self.name),Class='am-list-item-text')
-        item_text2  = HTML.DIV('时间：{}-{}'.format(self.startdate.strftime('%Y/%m/%d'),self.enddate.strftime('%Y/%m/%d')),Class='am-list-item-text')
-        item_text3  = HTML.DIV('展厅：{}'.format(self.hall),Class='am-list-item-text')
-        item_text4  = HTML.DIV('地址：{}'.format(self.addr),Class='am-list-item-text')
-        list_item  = HTML.LI('',Class='am-g am-list-item-desced')
-        list_item <= item_title
-        list_item <= item_text1
-        list_item <= item_text2
-        list_item <= item_text3
-        list_item <= item_text4
-        return list_item
-
 def add_place(name,abb,default=False):
     place_label = HTML.LABEL(name,Class='am-checkbox')
     if(default == True):
@@ -49,19 +34,31 @@ def get_file_text(file_name):
     fake_qs = '?foo={}'.format(time.time())
     return open(file_name+fake_qs).read()
 
+def gen_li(item):
+    item_title = HTML.A(item['show'],href=item['url'])
+    item_text1  = HTML.DIV('展馆：{}'.format(item['name']),Class='am-list-item-text')
+    item_text2  = HTML.DIV('时间：{}-{}'.format(item['start'],item['end']),Class='am-list-item-text')
+    item_text3  = HTML.DIV('展厅：{}'.format(item['hall']),Class='am-list-item-text')
+    item_text4  = HTML.DIV('地址：{}'.format(item['addr']),Class='am-list-item-text')
+    list_item  = HTML.LI('',Class='am-g am-list-item-desced')
+    list_item <= item_title
+    list_item <= item_text1
+    list_item <= item_text2
+    list_item <= item_text3
+    list_item <= item_text4
+    return list_item
+
 def refresh():
     expo_list = []
-    lines = get_file_text('sneic.json')
-    print("hello world!")
-    expo_json = json.loads(lines)
-    print(expo_json)
     document['expo_list'].clear()
-    for item in document.get(name="cbx"):
-        if(item.checked==True):
-            expo_list += get_expo_list(item.value)
+    lines = get_file_text('sneic.json')
+    expo_list = json.loads(lines)['2018']
+    print(expo_list)
+    #for item in document.get(name="cbx"):
+    #    if(item.checked==True):
+    #        expo_list += json_list.find(name==item.value)
     for expo in expo_list:
-        #expo.show_info()
-        document['expo_list'] <= expo.gen_li()
+        document['expo_list'] <= gen_li(expo)
 
 def onPlaceSelected(ev):
     refresh()
